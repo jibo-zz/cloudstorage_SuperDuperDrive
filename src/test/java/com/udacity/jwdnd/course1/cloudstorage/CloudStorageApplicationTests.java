7214835e2d499f4b2f5f6ad50d56ff256d355129
Mohamed Omar <m.jiboman@gmail.com>
2021-05-23 18:13:18 +0300

    uploading to github

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -0,0 +1,56 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CloudStorageApplicationTests {
+
+	protected WebDriver driver;
+	@LocalServerPort
+	protected int port;
+
+	@BeforeAll
+	static void beforeAll() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	public void beforeEach() {
+		this.driver = new ChromeDriver();
+	}
+
+	@AfterEach
+	public void afterEach() {
+		if (this.driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void getLoginPage() {
+		driver.get("http://localhost:" + this.port + "/login");
+		Assertions.assertEquals("Login", driver.getTitle());
+	}
+
+	protected HomePage signUpAndLogin() {
+		driver.get("http://localhost:" + this.port + "/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.setFirstName("Mohamed");
+		signupPage.setLastName("Cheikh");
+		signupPage.setUserName("jibo");
+		signupPage.setPassword("jibo");
+		signupPage.signUp();
+		driver.get("http://localhost:" + this.port + "/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.setUserName("jibo");
+		loginPage.setPassword("jibo");
+		loginPage.login();
+
+		return new HomePage(driver);
+	}
+}
